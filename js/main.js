b9a1688d1480a268c48b36c0be304df90b1f3e1a
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2025-05-27 19:57:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -409,32 +409,66 @@ window.addEventListener("load", () => {
   document.body.appendChild(script);
 }); */
 
-window.copilotChatConfig = {
-  buttonSrc: "https://backend.chatbase.co/storage/v1/object/public/chat-icons/4c5502be-6173-4a25-affc-11a7c7f4a6b6/NayFwE8gl8kgDuXaZoh3n.jpg",
-  iframeSrc: "https://copilotstudio.microsoft.com/environments/Default-373e90d2-d9a3-43b5-832e-c8e9847a79f8/bots/cr145_taniStatsChatbot/webchat?__version__=2",
-};
+   (function () {
+    if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+      window.chatbase = (...args) => {
+        if (!window.chatbase.q) {
+          window.chatbase.q = [];
+        }
+        window.chatbase.q.push(args);
+      };
+  
+      window.chatbase = new Proxy(window.chatbase, {
+        get(target, prop) {
+          if (prop === "q") {
+            return target.q;
+          }
+          return (...args) => target(prop, ...args);
+        },
+      });
+    }
+  
+    const onLoad = function () {
+      const script = document.createElement("script");
+      script.src = "https://www.chatbase.co/embed.min.js";
+      script.id = "cy2o6ilqRZeNoeiKkVhh8";
+      script.domain = "www.chatbase.co";
+      document.body.appendChild(script);
+    };
+  
+    if (document.readyState === "complete") {
+      onLoad();
+    } else {
+      window.addEventListener("load", onLoad);
+    }
+  })(); 
 
-window.addEventListener("load", () => {
-  const copilotIframe = document.createElement("iframe");
-  copilotIframe.setAttribute("id", "chatbot-container");
-  copilotIframe.setAttribute("src", window.copilotChatConfig.iframeSrc);
-  copilotIframe.setAttribute("frameborder", "0");
-  document.body.appendChild(copilotIframe);
+// window.copilotChatConfig = {
+//   buttonSrc: "https://backend.chatbase.co/storage/v1/object/public/chat-icons/4c5502be-6173-4a25-affc-11a7c7f4a6b6/NayFwE8gl8kgDuXaZoh3n.jpg",
+//   iframeSrc: "https://copilotstudio.microsoft.com/environments/Default-373e90d2-d9a3-43b5-832e-c8e9847a79f8/bots/cr145_taniStatsChatbot/webchat?__version__=2",
+// };
 
-  const copilotButtonImage = document.createElement("img");
-  copilotButtonImage.setAttribute("src", window.copilotChatConfig.buttonSrc);
-  copilotButtonImage.setAttribute("alt", "Chatbot");
+// window.addEventListener("load", () => {
+//   const copilotIframe = document.createElement("iframe");
+//   copilotIframe.setAttribute("id", "chatbot-container");
+//   copilotIframe.setAttribute("src", window.copilotChatConfig.iframeSrc);
+//   copilotIframe.setAttribute("frameborder", "0");
+//   document.body.appendChild(copilotIframe);
 
-  const copilotButton = document.createElement("button");
-  copilotButton.setAttribute("id", "chatbot-toggle");
-  copilotButton.appendChild(copilotButtonImage);
+//   const copilotButtonImage = document.createElement("img");
+//   copilotButtonImage.setAttribute("src", window.copilotChatConfig.buttonSrc);
+//   copilotButtonImage.setAttribute("alt", "Chatbot");
 
-  copilotButton.addEventListener("click", () => {
-    copilotIframe.style.display = copilotIframe.style.display === "none" ? "block" : "none";
-  });
+//   const copilotButton = document.createElement("button");
+//   copilotButton.setAttribute("id", "chatbot-toggle");
+//   copilotButton.appendChild(copilotButtonImage);
 
-  document.body.appendChild(copilotButton);
-});
+//   copilotButton.addEventListener("click", () => {
+//     copilotIframe.style.display = copilotIframe.style.display === "none" ? "block" : "none";
+//   });
+
+//   document.body.appendChild(copilotButton);
+// });
 
 // $(document).ready(function () {
 //   // Get today's date and subtract 1 day
